370c7c4ff519a39bc20162dafd96dcb653ece374
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-05 23:22:45 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105232016) do
+ActiveRecord::Schema.define(version: 20180105232231) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180105232016) do
   create_table "patients", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
 end
